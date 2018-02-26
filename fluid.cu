188bc5e058319d68adee6946685ad393cab4657b
PWhiddy <all.cows.like.to.moo@gmail.com>
2018-02-25 21:54:33 -0800

    rays sample density from 3D volume

--- fluid.cu
+++ fluid.cu
@@ -26,7 +26,16 @@ void save_image(uint8_t *pixels, dims img_dims, std::string name) {
 // Avoid reallocating volume buffer each step by swapping?
 void simulate_fluid(float *volume, dims dimensions, float time_step);
 
-
+__device__ inline float get_density(float3 p, int3 d, float* vol) {
+	int3 c = make_int3(p);
+	if (c.x < 0 || c.y < 0 || c.z < 0 ||
+	    c.x >= d.x || c.y >= d.y || c.z >= d.z) {
+		return 0.0;
+	} else {
+		return vol[c.z*d.y*d.x + c.y*d.x + c.x];
+	}
+	
+}
 
 __global__ void render_pixel( uint8_t *image, float *volume, 
 	dims img_dims, dims vol_dims, float step_size, 
@@ -36,34 +45,41 @@ __global__ void render_pixel( uint8_t *image, float *volume,
 	int y = blockDim.y*blockIdx.y+threadIdx.y;
 	if (x >= img_dims.x || y >= img_dims.y) return;
 	
+	int3 vd = make_int3(vol_dims.x, vol_dims.y, vol_dims.z);
 	// Create Normalized UV image coordinates
 	float uvx = float(x)/float(img_dims.x)-0.5;
 	float uvy = float(y)/float(img_dims.y)-0.5;
 	uvx *= float(img_dims.x)/float(img_dims.y);	
 
 	// Set up ray originating from camera
-	float3 pos = make_float3(cam_pos.x, cam_pos.y, cam_pos.z);
+	float3 ray_pos = make_float3(cam_pos.x, cam_pos.y, cam_pos.z);
 	float3 ray_dir = normalize(make_float3(uvx,uvy,0.4));
-
+	float3 dir_to_light = normalize(
+		make_float3(light_dir.x, light_dir.y, light_dir.z));
 	float accum = 0.0;
 	
 	// Trace ray through volume
 	for (int step=0; step<64; step++) {
-	// At each step, cast occlusion ray towards light source
-		float occlusion = 0.0;
+	// At each step, cast occlusion ray towards light source	
+		float3 occ_pos = ray_pos;
+		float occlusion = 1.0;
 		for (int occ=0; occ<32; occ++) {
-			occlusion += 0.1;
+			occlusion *= (1.0-get_density(occ_pos, vd, volume));
+			occ_pos += dir_to_light*step_size;
 		}
-		accum += occlusion;
+		accum += get_density(ray_pos, vd, volume)*occlusion;
+		ray_pos += ray_dir*step_size;
 	}
 
 	int pixel = 3*(y*img_dims.x+x);
-	image[pixel+0] = (uint8_t)(200.0*ray_dir.x);
-	image[pixel+1] = (uint8_t)(accum);
-	image[pixel+2] = (uint8_t)(200.0*ray_dir.z);
+	image[pixel+0] = (uint8_t)(50.0*accum);
+	image[pixel+1] = (uint8_t)(50.0*accum);
+	image[pixel+2] = (uint8_t)(50.0*accum);
 }
 
-void render_fluid(uint8_t *render_target, dims img_dims, float *d_volume /*pointer to gpu mem?*/, dims vol_dims, float step_size, fdims light_dir, fdims cam_pos, float rotation) {
+void render_fluid(uint8_t *render_target, dims img_dims, 
+	float *d_volume /*pointer to gpu mem?*/, dims vol_dims, 
+	float step_size, fdims light_dir, fdims cam_pos, float rotation) {
 
 	float measured_time=0.0f;
 	cudaEvent_t start, stop;
@@ -116,7 +132,8 @@ __global__ void kernel_A( float *g_data, int dimx, int dimy )
     g_data[idx] = value;
 }
 
-float run_kernel( void (*kernel)( float*, int,int), float *d_data, int dimx, int dimy, int nreps, int blockx, int blocky )
+float run_kernel( void (*kernel)( float*, int,int), float *d_data, 
+	int dimx, int dimy, int nreps, int blockx, int blocky )
 {
 	float measured_time=0.0f;
 	cudaEvent_t start, stop;
