1577d442b94652a7547937d7a3223a5794822330
PWhiddy <all.cows.like.to.moo@gmail.com>
2018-02-25 19:03:23 -0800

    wrapper to launch image rendering kernel

--- fluid.cu
+++ fluid.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 struct dims {
     int x, y, z;
@@ -10,13 +11,51 @@ struct fdims {
     float x, y, z;
 };
 
-void render_fluid(uint8_t *render_target, dims img_dims, float *volume /*pointer to gpu mem?*/, dims vol_dims, fdims cam_pos, float rotation);
+void render_fluid(uint8_t *render_target, dims img_dims, float *d_volume /*pointer to gpu mem?*/, dims vol_dims, float step_size, fdims light_dir, fdims cam_pos, float rotation) {
+
+	float measured_time=0.0f;
+	cudaEvent_t start, stop;
+	cudaEventCreate( &start );
+	cudaEventCreate( &stop  );
+
+	dim3 block( 32, 32 );
+	dim3 grid( (img_dims.x+32-1)/32, (img_dims.y+32-1)/32 );
+
+	cudaEventRecord( start, 0 );
+	
+	// Allocate device memory for image
+	int img_bytes = sizeof(uint8_t)*img_dims.x*img_dims.y;
+	uint8_t *device_img;
+	cudaMalloc( (void**)&device_img, img_bytes );
+        if( 0 == device_img )
+        {
+                printf("couldn't allocate GPU memory\n");
+   		return;
+        }
+
+	//render_pixel<<<grid,block>>>( 
+	//	device_img, d_volume, img_dims, vol_dims, 
+	//	step_size, light_dir, cam_pos, rotation);
+
+	// Read image back
+	cudaMemcpy( render_target, device_img, img_bytes, cudaMemcpyDeviceToHost );
+
+	cudaEventRecord( stop, 0 );
+	cudaThreadSynchronize();
+	cudaEventElapsedTime( &measured_time, start, stop );
+
+	cudaEventDestroy( start );
+	cudaEventDestroy( stop );
+
+	std::cout << "Render Time: " << measured_time << "\n";
+	cudaFree(device_img);
+}
 
 void save_image(uint8_t *pixels, dims img_dims, std::string name) {
     std::ofstream file(name, std::ofstream::binary);
     if (file.is_open()) {
         file << "P6\n" << img_dims.x << " " << img_dims.y << "\n" << "255\n";
-        file.write(pixels, img_dims.x*img_dims.y*3);
+        file.write((char *)pixels, img_dims.x*img_dims.y*3);
         file.close();
     } else {
         std::cout << "Could not open file :(\n";
