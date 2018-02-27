19fb4825dcd4d9a6f8671fc47bf9b38a919a7de3
PWhiddy <all.cows.like.to.moo@gmail.com>
2018-02-26 17:40:57 -0800

    added int in3 sub operator

--- cutil_math.h
+++ cutil_math.h
@@ -607,6 +607,11 @@ inline __host__ __device__ int3 operator-(int3 a, int3 b)
     return make_int3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
+inline __host__ __device__ int3 operator-(int3 a, int b)
+{
+    return make_int3(a.x-b, a.y-b, a.z-b);
+}
+
 inline __host__ __device__ void operator-=(int3 &a, int3 b)
 {
     a.x -= b.x; a.y -= b.y; a.z -= b.z;
