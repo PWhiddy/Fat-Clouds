886679caa773de700953b8c60dd43c16da279e90
PWhiddy <all.cows.like.to.moo@gmail.com>
2018-03-17 22:32:29 -0700

    added cuda path variable and cudart

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-nvcc fluid.cu -std=c++11 -Wno-deprecated-gpu-targets -o run_fluid
+#!/bin/sh
+
+CUDA_DIR='usr/local/cuda-8.0'
+
+nvcc fluid.cu -std=c++11 -I ${CUDA_DIR}/include -L ${CUDA_DIR}/lib64 -Wno-deprecated-gpu-targets -o run_fluid
