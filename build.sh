3479d262cd3551bb0f8bdef192a9ab033fef16e2
Peter Jasper Whidden <pdubs@epyc.astro.washington.edu>
2018-03-21 09:01:53 -0700

    use specified version of nvcc

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 CUDA_DIR='usr/local/cuda-8.0'
 
-nvcc fluid.cu -std=c++11 -I ${CUDA_DIR}/include -L ${CUDA_DIR}/lib64 -Wno-deprecated-gpu-targets -o run_fluid
+${CUDA_DIR}/bin/nvcc fluid.cu -std=c++11 -I ${CUDA_DIR}/include -L ${CUDA_DIR}/lib64 -Wno-deprecated-gpu-targets -o run_fluid
