4e0f3f9d34ff6e671332727ea45ea4b5e5a1981d
Peter Jasper Whidden <pdubs@epyc.astro.washington.edu>
2018-03-27 03:56:04 -0700

    fixed path to cuda

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CUDA_DIR='usr/local/cuda-8.0'
+CUDA_DIR='usr/local/cuda-9.1'
 
-${CUDA_DIR}/bin/nvcc fluid.cu -std=c++11 -I ${CUDA_DIR}/include -L ${CUDA_DIR}/lib64 -Wno-deprecated-gpu-targets -o run_fluid
+/${CUDA_DIR}/bin/nvcc fluid.cu -std=c++11 -I ${CUDA_DIR}/include -L ${CUDA_DIR}/lib64 -Wno-deprecated-gpu-targets -o run_fluid
